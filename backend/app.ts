f4144861cb021fc120b7d56e524e4c494e53a092
LuisDiegoMora98 <aguilarluisdi@estudiantec.cr>
2022-04-18 19:24:18 -0600

    Backend db connection + tweaks

--- backend/app.ts
+++ backend/app.ts
@@ -3,26 +3,51 @@ import morgan from 'morgan';
 import cors from 'cors';
 import path from 'path';
 import history from 'connect-history-api-fallback';
+import mongoose, { ConnectOptions } from 'mongoose';
 
-const app = express();
+class App {
 
-// Middleware
-app.use(morgan('tiny'));
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+  private app = express();
 
-// Middleware para Vue.js router modo history
+  constructor() {
+    this.configMiddleware();
+    this.configRoutes()
+    this.connectDatabase();
+    this.startServer();
+  }
 
-app.use(history());
-app.use(express.static(path.join(__dirname, 'public')));
+  private configMiddleware(){
+    // Middleware
+    this.app.use(morgan('tiny'));
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
 
-// Rutas
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+    // Middleware para Vue.js router modo history
+    this.app.use(history());
+    this.app.use(express.static(path.join(__dirname, 'public')));
+  }
 
-app.set('puerto', process.env.PORT || 3000);
-app.listen(app.get('puerto'), () => {
-  console.log('Example app listening on port: '+ app.get('puerto'));
-});
+  private configRoutes(){
+    // Rutas
+    this.app.get('/', (req, res) => {
+      res.send('Hello World!');
+    });
+  }
+
+  private async connectDatabase(){
+    const uri = 'mongodb://localhost:27017';
+    const options = {useNewUrlParser: true, useUnifiedTopology: true};
+    await mongoose.connect(uri, options as ConnectOptions);
+  }
+
+  private startServer(){
+    this.app.set('puerto', process.env.PORT || 3000);
+    this.app.listen(this.app.get('puerto'), () => {
+      console.log('Example app listening on port: '+ this.app.get('puerto'));
+    });
+  }
+
+}
+
+new App();
