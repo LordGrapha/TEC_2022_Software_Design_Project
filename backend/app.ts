1f53dbccbe1ab325903291f2c8f225def2045fba
LuisDiegoMora98 <aguilarluisdi@estudiantec.cr>
2022-04-28 18:02:18 -0600

    coneccion a la nube

--- backend/app.ts
+++ backend/app.ts
@@ -12,8 +12,10 @@ class App {
   constructor() {
     this.configMiddleware();
     this.configRoutes()
-    this.connectDatabase();
-    this.startServer();
+    //Once database is connected, then it starts the server
+    this.connectDatabase().then(() => {
+      this.startServer();
+    });  
   }
 
   private configMiddleware(){
@@ -36,15 +38,17 @@ class App {
   }
 
   private async connectDatabase(){
-    const uri = 'mongodb://localhost:27017';
+    const uri = 'mongodb+srv://test:test@teccluster.ke5kh.mongodb.net/sisparking?retryWrites=true&w=majority';
     const options = {useNewUrlParser: true, useUnifiedTopology: true};
     await mongoose.connect(uri, options as ConnectOptions);
+    console.log("Connected to MongoDB");
+    
   }
 
   private startServer(){
-    this.app.set('puerto', process.env.PORT || 3000);
+    this.app.set('port', process.env.PORT || 3000);
     this.app.listen(this.app.get('puerto'), () => {
-      console.log('Example app listening on port: '+ this.app.get('puerto'));
+      console.log('Example app listening on port: '+ this.app.get('port'));
     });
   }
 
