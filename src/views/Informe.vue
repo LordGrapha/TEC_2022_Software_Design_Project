5eb8029e9d77bc174b1eb8c89036aac43f984a4f
Jorge Jaén <jbjrjaen@gmail.com>
2022-05-07 19:28:00 -0600

    Create Informe.vue

---
+++ src/views/Informe.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div>
+            <b-buttom variant="todos">Todos los estacionamientos</b-buttom>
+        </div>        
+        <div>
+            <b-buttom variant="funcionarios">Funcionarios de la institución</b-buttom>
+        </div>        
+        <div>
+            <b-buttom variant="estadistica">Estadistica franjas horarias</b-buttom>
+        </div>        
+        <div>
+            <b-buttom variant="funcionari_cedula">Funcionario por cedula</b-buttom>
+        </div>        
+
+    </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'reptodos',
+
+}
+</script>
