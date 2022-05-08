2bf879eeba2ceaa61b3a7741d0b8ea64acb9c006
JoseDanielRojas <43434891+JoseDanielRojas@users.noreply.github.com>
2022-05-08 09:08:09 -0600

    Direccion Parqueos

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,11 +5,13 @@ import Informes from '@/views/Informes.vue'
 import Gestion from '@/views/Gestion.vue'
 import Menu from '@/components/Menu.vue'
 import Reservacion from '@/components/Reservacion.vue'
+import Parqueos from '@/components/Parqueos.vue'
 
 const routes: Array<RouteRecordRaw> = [
   { path: '/', component: Login },
   { path: '/menu', component: Menu },
   { path: '/gestion_reserva', component: Reservacion },
+  { path: '/consulta_parqueos', component: Parqueos },
   { path: '/informes', component: Informes },
   { path: '/gestion', component: Gestion },
 ]
